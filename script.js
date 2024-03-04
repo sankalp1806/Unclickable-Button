e83eb8a64e4ce8de42167203cb649700144e96e3
sankalp1806 <132252437+sankalp1806@users.noreply.github.com>
2024-03-04 23:40:52 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'Hurrayyyyyyyy <br> I Love You Too :)';
+    wrapper.style.backgroundImage = "url('Image.jpg')"; // Change 'your_new_background_image.jpg' with your image path
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
